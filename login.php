d3dff553685c3285b02f6bd47de3893501cb688c
Hadi14 <asusn43@yahoo.com>
2023-08-08 13:41:20 +0330

    k678

--- login.php
+++ login.php
@@ -5,13 +5,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="base.css">
 </head>
 
 <body>
-    <?
-    echo '<span class="btnblue">login</span>';
-    ?>
+    <div class="tac">
+        <img style="width: 200px;" src="images/notes.png" alt=""><br>
+        <form action="" ">
+            <label for="">UserName</label>
+            <input type=" text"><br><br>
+            <label for="">Password</label>
+            <input type="password"><br><br>
+            <input class="btnblue" type="submit" value="ُSend">
+        </form>
+    </div>
 </body>
 
 </html>
