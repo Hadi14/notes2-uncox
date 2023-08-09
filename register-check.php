1462cd96b05d2ad8f5378d86c77a3bfe1f8cc0d6
Hadi14 <asusn43@yahoo.com>
2023-08-09 10:19:06 +0330

    register

--- register-check.php
+++ register-check.php
@@ -4,23 +4,29 @@ $db = Db::getInstance();
 $u = $_POST['uname'];
 $p = $_POST['pass'];
 $cp = $_POST['cpass'];
+echo $_POST['uname'];
+br();
+echo $_POST['pass'];
+br();
+echo $_POST['cpass'];
+br();
 
-$sql = "select * from users where username='$u' and password='$p'";
+
+$sql = "select * from users where username='$u' and password='$p' ";
 $record = $db->query($sql);
 
 if ($u == "" || $p == "" || $cp == "") {
     echo "Plase Fill All Boxes.!!!";
 } else if ($p != $cp) {
     echo "Plase type the same Password and Confirm Password.!!!";
+} else if ($record != null) {
+    echo "You Already Registerd.!!!";
+} else {
+    $sql = "insert into users (username, password,level) values ('$u','$p','2') ";
+    $res = $db->insert($sql);
+    if ($res) {
+        echo "Thankyou You Registeded.!!!";
+    } else {
+        echo "Register in Error.!!!";
+    }
 }
-
-
-
-
-
-
-// if ($record == null) {
-//     header("Location: fail.php");
-// } else {
-//     header("Location: success.php");
-// }
